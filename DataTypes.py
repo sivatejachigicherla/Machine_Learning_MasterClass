f49a7b49ec887363e61bf275f46804161d849d38
Shiva Teja Chigicherla <shiva000teja@gmail.com>
2020-08-14 20:03:41 +0530

    1st commit

---
+++ DataTypes.py
@@ -0,0 +1,106 @@
+a=10  
+b="Hi Python"  
+c = 10.5  
+print(type(a))  
+print(type(b))  
+print(type(c))  
+
+a = 5  
+print("The type of a", type(a))  
+  
+b = 40.5  
+print("The type of b", type(b))  
+  
+c = 1+3j  
+print("The type of c", type(c))  
+print(" c is a complex number", isinstance(1+3j,complex))  
+
+str = "string using double quotes"  
+print(str)  
+s = '''''A multiline 
+string'''  
+print(s)  
+
+str1 = 'hello javatpoint' #string str1    
+str2 = ' how are you' #string str2    
+print (str1[0:2]) #printing first two character using slice operator    
+print (str1[4]) #printing 4th character of the string    
+print (str1*2) #printing the string twice    
+print (str1 + str2) #printing the concatenation of str1 and str2    
+
+list1  = [1, "hi", "Python", 2]    
+#Checking type of given list  
+print(type(list1))  
+  
+#Printing the list1  
+print (list1)  
+  
+# List slicing  
+print (list1[3:])  
+  
+# List slicing  
+print (list1[0:2])   
+  
+# List Concatenation using + operator  
+print (list1 + list1)  
+  
+# List repetation using * operator  
+print (list1 * 3)  
+
+
+tup  = ("hi", "Python", 2)    
+# Checking type of tup  
+print (type(tup))    
+  
+#Printing the tuple  
+print (tup)  
+  
+# Tuple slicing  
+print (tup[1:])    
+print (tup[0:1])    
+  
+# Tuple concatenation using + operator  
+print (tup + tup)    
+  
+# Tuple repatation using * operator  
+print (tup * 3)     
+  
+# Adding value to tup. It will throw an error.  
+#t[2] = "hi"  
+
+
+
+d = {1:'Jimmy', 2:'Alex', 3:'john', 4:'mike'}     
+  
+# Printing dictionary  
+print (d)  
+  
+# Accesing value using keys  
+print("1st name is "+d[1])   
+print("2nd name is "+ d[4])    
+  
+print (d.keys())    
+print (d.values())
+
+
+# Python program to check the boolean type  
+print(type(True))  
+print(type(False))  
+
+
+# Creating Empty set  
+set1 = set()  
+  
+set2 = {'James', 2, 3,'Python'}  
+  
+#Printing Set value  
+print(set2)  
+  
+# Adding element to the set  
+  
+set2.add(10)  
+print(set2)  
+  
+#Removing element from the set  
+set2.remove(2)  
+print(set2)  
---
+++ Variables.py
@@ -0,0 +1,36 @@
+x = 5
+y = "Hello, World!"
+z = 2.69
+print(x)
+print(y)
+print(z)
+
+a = 50  
+b = a  
+print(id(a))  
+print(id(b))  
+# Reassigned variable a  
+a = 500  
+print(id(a))  
+
+
+name = "A"  
+Name = "B"  
+naMe = "C"  
+NAME = "D"  
+n_a_m_e = "E"  
+_name = "F"  
+name_ = "G"  
+_name_ = "H"  
+na56me = "I"  
+  
+print(name,Name,naMe,NAME,n_a_m_e, NAME, n_a_m_e, _name, name_,_name, na56me)  
+
+
+x=y=z=50    
+print(x)    
+print(y)    
+print(z)
+
+a,b,c=5,10,15    
+print (a,b,c)
